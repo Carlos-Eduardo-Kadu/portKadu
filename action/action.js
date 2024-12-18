5cc2ef6c0ac3603799af7f9142b917673b5f23c0
Carlos-Eduardo-Kadu <kadumeloduarte@gmail.com>
2024-12-18 13:01:52 -0300

    Adicionei videos

--- action/action.js
+++ action/action.js
@@ -1,9 +1,10 @@
 const mediaSets = {
     1: [
       { type: "video", src: "videos/Advocacia Barreto -  gancho 1.1.mp4" },
+      { type: "video", src: "videos/Criativo - 2.mp4" },
       { type: "video", src: "videos/ads 39.mp4" },
       { type: "video", src: "videos/ice cube.mp4" },
-      { type: "video", src: "videos/Diferencial do gordon.mp4" },
+      { type: "video", src: "videos/criativo 3 - final.mp4" },
       { type: "video", src: "videos/purple burrn - 2.mp4" },
       { type: "video", src: "videos/the who.mp4" },
       { type: "video", src: "videos/video teste - 2.mp4" },
